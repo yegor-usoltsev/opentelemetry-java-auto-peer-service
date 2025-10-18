4a15d04bfdc6dbfd2ff6083c9720fdbd5ae48c67
Yegor Usoltsev <yegor@usoltsev.xyz>
2025-10-18 14:31:27 +0300

    @SuppressWarnings("unchecked")

--- src/test/java/xyz/usoltsev/opentelemetry/AutoPeerServiceSpanProcessorTest.java
+++ src/test/java/xyz/usoltsev/opentelemetry/AutoPeerServiceSpanProcessorTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
+@SuppressWarnings("unchecked")
 class AutoPeerServiceSpanProcessorTest {
 
     private AutoPeerServiceSpanProcessor processor;
